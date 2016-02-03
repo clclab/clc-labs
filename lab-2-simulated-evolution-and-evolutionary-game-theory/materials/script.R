f965a2e0f743fc16fb39616fe50b01c8e87cb706
Dieuwke Hupkes <dieuwkehupkes@gmail.com>
2016-02-03 14:03:24 +0100

    schrijf script voor deel 1 lab 2

--- lab-2-simulated-evolution-and-evolutionary-game-theory/materials/script.R
+++ lab-2-simulated-evolution-and-evolutionary-game-theory/materials/script.R
@@ -2,28 +2,29 @@
 library(stringr)
 
 # generate random strings
-x = randomString(maxLenth=10, minLength=10, alphabet=c('A','G','C','U'))
+x = paste(sample(c('A','G','C','U'), size=10, replace=TRUE), collapse='')
 
 simulate_evolution <- function(size_population=100, size_individual=100, sim_length=100, mu=0) {
 
     # generate initial population with random strings
     population <- rep(0,size_population)        # generate empty string
     for (i in 1:size_population) {
-        population[i] <- randomString(maxLenth=size_individual, minLength=size_individual, alphabet=c('A','G','C','U'))
+        population[i] <- paste(sample(c('A','G','C','U'), size=size_individual, replace=TRUE), collapse='')
     }
 
+    # compute fitness
     fitness <- rep(0,size_population)
-        for (i in 1:size_population) {fitness[i] <- str_count(population[i], "CAC")}
+    for (i in 1:size_population) {fitness[i] <- str_count(population[i], "CAC")}
 
-    # simulate evolution
     av_fitness = rep(0, sim_length)
     av_fitness[1] <- mean(fitness)
+
+    # simulate evolution
     for (j in 2:sim_length) {
 
         # generate children. 
         for (i in 1:size_population) {
-            # I will have to find out how to use the "random" package
-            # on my system
+            population = sample(population, size=size_population, replace=TRUE, prob = fitness/sum(fitness))
 
             # mutation children
         }
@@ -33,10 +34,12 @@ simulate_evolution <- function(size_population=100, size_individual=100, sim_len
 
         # add to list with average fitness
         av_fitness[j] <- mean(fitness)
-
-        # plot average fitness
-        generation = seq(1,100,1)
-        plot(av_fitness, generation, type="o",ann=false)
-        title(main="Average population fitness", xlab="xlab", ylab="ylab")
     }
+
+    # plot average fitness
+    generation = seq(1,100,1)
+    plot(generation, av_fitness, type="l",ann=FALSE)
+    title(main="Average population fitness", xlab="Generation", ylab="Fitness")
 }
+
+simulate_evolution(size_population=100, size_individual=100, sim_length=100, mu=0)
